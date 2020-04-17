47c7672605e019fa6322cc87482200ec124597cc
cuiyuanqi <12345678>
2020-04-17 17:16:22 +0800

    Initial commit

---
+++ src/main/java/com/example/demo/controller/DataController.java
@@ -0,0 +1,12 @@
+package com.example.demo.controller;
+
+import org.springframework.data.redis.core.RedisTemplate;
+
+public class DataController {
+
+    public static void main(String[] args) {
+        RedisTemplate<Object, Object> objectObjectRedisTemplate = new RedisTemplate<>();
+    }
+    
+    
+}
