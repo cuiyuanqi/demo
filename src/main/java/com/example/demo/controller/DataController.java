4bd70bbb9fd7ae1cc6cae5b1e291944fbb3bed2b
cuiyuanqi <12345678>
2020-04-17 20:03:22 +0800

    修改

--- src/main/java/com/example/demo/controller/DataController.java
+++ src/main/java/com/example/demo/controller/DataController.java
@@ -1,11 +1,13 @@
 package com.example.demo.controller;
 
+import com.sun.org.apache.xpath.internal.operations.String;
 import org.springframework.data.redis.core.RedisTemplate;
 
 public class DataController {
 
     public static void main(String[] args) {
         RedisTemplate<Object, Object> obje1ctObjectRedisTemplate = new RedisTemplate<>();
+        String string = new String();
     }
     
     
